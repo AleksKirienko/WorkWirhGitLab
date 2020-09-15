7f29092c61d9d6c2de3d6ba44d8a3b8378e606ac
AleksKirienko <aleks.Kirienko@bk.ru>
2020-09-15 23:24:50 +0700

    Added kdoc

--- app/src/main/java/ru/sibsutis/workwithgitlab/Functions.kt
+++ app/src/main/java/ru/sibsutis/workwithgitlab/Functions.kt
@@ -1,11 +1,30 @@
 package ru.sibsutis.workwithgitlab
 
+/**
+ * Класс, содержащий функции поиска минимального и максимального чисел
+ * @author Alexandra Gervas
+ *
+ */
 class Functions {
+    /**
+     * Функция поиска минимального числа
+     *
+     * @param a - первое число
+     * @param b - второе число
+     * @return минимальное число a или b
+     */
     public fun myMin(a: Int, b: Int): Int {
         return if (a < b) a
         else b
     }
 
+    /**
+     * Функция поиска максимального числа
+     *
+     * @param a - первое число
+     * @param b - второе число
+     * @return максимальное число a или b
+     */
     public fun myMax(a: Int, b: Int): Int {
         return if (a > b) a
         else b
