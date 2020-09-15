304b66e40e0a723539e3309c780e73c01e6ebb40
AleksKirienko <aleks.Kirienko@bk.ru>
2020-09-15 20:18:23 +0700

    First commit

---
+++ app/src/main/java/ru/sibsutis/workwirhgitlab/Functions.kt
@@ -0,0 +1,13 @@
+package ru.sibsutis.workwirhgitlab
+
+class Functions {
+    public fun myMin(a: Int, b: Int): Int {
+        return if (a < b) a
+        else b
+    }
+
+    public fun myMax(a: Int, b: Int): Int {
+        return if (a > b) a
+        else b
+    }
+}
